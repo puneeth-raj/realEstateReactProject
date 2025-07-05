42b7324e6a7a630ddd6bb4687fd98f8398572bb9
puneeth-raj <puneeth.panayal@gmail.com>
2025-07-05 20:05:56 +0530

    contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 // import React from 'react'
 
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Header from "./components/Header"
 import Project from "./components/Project"
 import Testimonials from "./components/Testimonials"
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
-      
+      <ToastContainer/>
       <Header/>
       <About/>
       <Project/>
       <Testimonials/>
+      <Contact/>
     </div>
   )
 }
